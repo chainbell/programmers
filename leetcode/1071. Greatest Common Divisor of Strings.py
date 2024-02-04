a861bc8400226d3739193080463693ade021f88e
yoonsanggyu <chainbell@gmail.com>
2024-02-04 18:55:59 +0900

    최대 공약수 gcd 함수를 문자열에 적용

--- leetcode/1071. Greatest Common Divisor of Strings.py
+++ leetcode/1071. Greatest Common Divisor of Strings.py
@@ -11,12 +11,19 @@ class Solution:
         gcd_length = a
         return str1[:gcd_length]
 
-
+    def gcdOfStrings2(self, str1: str, str2: str) -> str:
+        if str1 + str2 != str2 + str1:
+            return ''
+        for i in range(len(str1), 0, -1):
+            if len(str1) % i == 0 and len(str2) % i == 0:
+                return str1[:i]
 s = Solution()
 
 # Test cases
 str1 = "ABCABC"
 str2 = "ABC"
 
-print(s.gcdOfStrings(str1, str2))  # Output: "ABC"
+print(s.gcdOfStrings(str1, str2))
+
+print(s.gcdOfStrings2(str1, str2))
 
